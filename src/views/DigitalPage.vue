626f3f70586fb317d62aea214d8596c179542132
Alex33-cmd <62179831+Alex33-cmd@users.noreply.github.com>
2022-05-06 21:38:40 +0300

    unnecessary (practice) component removed

--- src/views/DigitalPage.vue
+++ src/views/DigitalPage.vue
@@ -2,27 +2,61 @@
   <div class="blue-grey darken-4">
       <h1 class="pa-4 white--text" >Digital Photography</h1>
       <v-row class="ma-2">
-        <DigitalPhoto 
+        <v-col
           v-for="photo in photos" v-bind:key="photo.id"
-          :photo="photo"
-          @openPhoto="openPhoto"
-        />
+          :photo="photo" 
+          ma-4
+          >
+              <v-card
+              class="mx-auto blue-grey darken-2"
+              max-width="250"
+              @click="openPhoto(photo)"
+              >
+                  <v-img
+                  max-height="167"
+                  class="white--text align-end"
+                  v-bind:src="photo.download_url"
+                  lazy-src="https://picsum.photos/id/11/100/60"
+                  >
+                      <template v-slot:placeholder>
+                        <v-row
+                          class="fill-height ma-0"
+                          align="center"
+                          justify="center"
+                        >
+                          <v-progress-circular
+                            indeterminate
+                            color="grey lighten-5"
+                          ></v-progress-circular>
+                        </v-row>
+                      </template>
+                      <v-card-title>{{photo.id}}</v-card-title>
+                  </v-img>
+
+                  <v-card-subtitle class="pb-0 white--text">
+                      {{photo.author}}
+                  </v-card-subtitle>
+
+                  <v-card-text class="white--text">
+                      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                  </v-card-text>
+              </v-card>
+          </v-col>
       </v-row>
       <PhotoLightbox :photo="currentPhoto" v-model="lightboxVisible"/>
   </div>
 </template>
 
 <script>
-import DigitalPhoto from "@/components/DigitalPhoto.vue"
 import PhotoLightbox from "@/components/PhotoLightbox.vue"
 import axios from "axios";
 
 export default {
   components: {
-    DigitalPhoto,
     PhotoLightbox
   },
   data: () => ({
+    isActive: false,
     photos: [],
     currentPhoto: {},
     lightboxVisible: false
@@ -33,7 +67,7 @@ export default {
   methods: {
     fetchPhoto() {
       // axios.get('https://jsonplaceholder.typicode.com/photos?_limit=16')
-      axios.get('https://picsum.photos/v2/list?limit=20')
+      axios.get('https://picsum.photos/v2/list?limit=60')
       .then(response => this.photos = response.data)
     },
     openPhoto(photo) {
